e782c44990298a99f2e2298328a8ba376e0d8053
zhangyaolun <987073229@qq.com>
2017-10-18 16:30:58 +0800

    18

--- caifei/js/oderShop.js
+++ caifei/js/oderShop.js
@@ -2,8 +2,7 @@ document.getElementsByTagName("body")[0].setAttribute("style","display:block");
 var o = {
 	init:function(){
 		FastClick.attach(document.body);
-		$('#pri_input input')[0].focus();
-		return;
+		$('.pri_input input').focus();
 		var data = JSON.parse(getParameter('result').replace(/'/g, '"'));
 		console.log(data)
 		console.log(getParameter('num'))
